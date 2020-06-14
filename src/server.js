bb1a906800c8445ad05125e4a78b916f4c5b6574
Stefan <jovanovicstefan69@gmail.com>
2020-06-14 14:06:23 -0600

    node js server complete and sending the website correctly

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+const port = 3000
+
+var htmlPath = path.join(__dirname, '../public');
+
+app.use(express.static(htmlPath));
+
+var server = app.listen(3000, function () {
+    var host = 'localhost';
+    var port = server.address().port;
+    console.log('listening on http://'+host+':'+port+'/');
+});
