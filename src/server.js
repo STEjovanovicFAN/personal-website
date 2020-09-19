32629cb8ca6ef20b5856a1670f4f0f2599c3b52a
Stefan <jovanovicstefan69@gmail.com>
2020-09-19 13:58:36 -0600

    changing the node code to be that of the sample

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express')
 const path = require('path');
 const app = express()
@@ -12,3 +13,15 @@ var server = app.listen(3000, function () {
     var port = server.address().port;
     console.log('listening on http://'+host+':'+port+'/');
 });
+*/
+
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, '../public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
